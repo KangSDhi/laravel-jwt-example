785e045d2c53723334b97cfdd9a54f637cb24466
KangSDhi <kangteknisi@gmail.com>
2023-01-16 19:23:03 +0700

    Membuat RESTful API Login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,8 @@ use App\Http\Controllers\Api\RegisterController;
 |
 */
 Route::post('register', RegisterController::class);
+
+Route::post('login', LoginController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
